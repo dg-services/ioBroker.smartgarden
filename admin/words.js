9094f33f793ce96ca859aba3e3fc4f1867c1a5a1
dg-services <code@dg-home.de>
2020-01-27 19:14:10 +0000

    Erstellen der Admin-GUI

--- admin/words.js
+++ admin/words.js
@@ -14,28 +14,28 @@ systemDictionary = {
 		'pl': 'Ustawienia adaptera dla smartgarden',
 		'zh-cn': 'smartgarden的适配器设置'
 	},
-	'option1': {
-		'en': 'option1',
-		'de': 'Option 1',
-		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'
+	'user': {
+		'en': 'User',
+		'de': 'Benutzer'
 	},
-	'option2': {
-		'en': 'option2',
-		'de': 'Option 2',
-		'ru': 'option2',
-		'pt': 'opção 2',
-		'nl': 'Optie 2',
-		'fr': 'Option 2',
-		'it': 'opzione 2',
-		'es': 'opcion 2',
-		'pl': 'Opcja 2',
-		'zh-cn': '选项2'
+	'passwd': {
+		'en': 'Password',
+		'de': 'Passwort'
+	}
+	'appKey': {
+		'en': 'Secret Application Key',
+		'de': 'Geheimer Applikation Key'
+	}
+	'appSecret': {
+		'en': 'Secret Application Password',
+		'de': 'Geheimes Applikation Passwort'
+	}
+	'authKey': {
+		'en': 'Authentication Key (provided by Husqvarna)',
+		'de': 'Authentikation Schlüssel (von Husqvarna)'
+	}
+	'authState': {
+		'en': 'Authentication State (provided by Husqvarna)',
+		'de': 'Authentikation State (von Husqvarna)'
 	}
 };
